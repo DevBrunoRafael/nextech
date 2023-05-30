05e7c1e194aad3c9388be16abe83dd1dfef04bba
Bruno Rafael <brunorafaeldls2016@gmail.com>
2023-05-30 16:35:51 -0300

    feat: 🚀 starting project

---
+++ src/main/java/com/nextech/io/nextech/NextechApplication.java
@@ -0,0 +1,13 @@
+package com.nextech.io.nextech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextechApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(NextechApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/nextech/io/nextech/NextechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextech.io.nextech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextechApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
